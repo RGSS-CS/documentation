95ac636e27c6ac12bd6ee9f9053b2c138e425e89
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-06-20 22:44:16 -0400

    Update baseUrl in docusaurus.config.ts for GitHub Pages deployment

--- williams-rgss-prod-docs/docusaurus.config.ts
+++ williams-rgss-prod-docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://docs.rgsscs.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/documentation/williams-rgss-prod-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
