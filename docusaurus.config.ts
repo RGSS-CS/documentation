2ae9147314004093cef03ff5141c19516734d89b
dave <d4nplus6@gmail.com>
2026-06-24 14:08:13 -0400

    Fixed indentation in docusarus config to accurately depict heirarchy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,29 +40,27 @@ const config: Config = {
 		'classic',
 		{
 			docs: {
-			sidebarPath: './sidebars.ts',
-			// Please change this to your repo.
-			// Remove this to remove the "edit this page" links.
-			editUrl:
-				'https://github.com/RGSS-CS/documentation',
+				sidebarPath: './sidebars.ts',
+				// Please change this to your repo.
+				// Remove this to remove the "edit this page" links.
+				editUrl: 'https://github.com/RGSS-CS/documentation',
 			},
 			blog: {
-			showReadingTime: true,
-			feedOptions: {
-				type: ['rss', 'atom'],
-				xslt: true,
-			},
-			// Please change this to your repo.
-			// Remove this to remove the "edit this page" links.
-			editUrl:
-				'https://github.com/RGSS-CS/documentation',
-			// Useful options to enforce blogging best practices
-			onInlineTags: 'warn',
-			onInlineAuthors: 'warn',
-			onUntruncatedBlogPosts: 'warn',
+				showReadingTime: true,
+				feedOptions: {
+					type: ['rss', 'atom'],
+					xslt: true,
+				},
+				// Please change this to your repo.
+				// Remove this to remove the "edit this page" links.
+				editUrl: 'https://github.com/RGSS-CS/documentation',
+				// Useful options to enforce blogging best practices
+				onInlineTags: 'warn',
+				onInlineAuthors: 'warn',
+				onUntruncatedBlogPosts: 'warn',
 			},
 			theme: {
-			customCss: './src/css/custom.css',
+				customCss: './src/css/custom.css',
 			},
 		} satisfies Preset.Options,
 		],
@@ -72,73 +70,77 @@ const config: Config = {
 		// Replace with your project's social card
 		image: 'img/docusaurus-social-card.jpg',
 		colorMode: {
-		respectPrefersColorScheme: true,
+			respectPrefersColorScheme: true,
 		},
 		navbar: {
-		title: 'My Site',
-		logo: {
-			alt: 'RGSS CS Club Logo',
-			src: 'img/logo.png',
-		},
-		items: [
-			{
-			type: 'docSidebar',
-			sidebarId: 'tutorialSidebar',
-			position: 'left',
-			label: 'Tutorial',
+			title: 'RGSS CS Docs',
+			logo: {
+				alt: 'RGSS CS Club Logo',
+				src: 'img/logo.png',
 			},
-			{to: '/blog', label: 'Blog', position: 'left'},
-			{
-				href: 'https://github.com/RGSS-CS/',
-				label: 'GitHub',
-				position: 'right',
-			},
-		],
+			items: [
+				{
+					type: 'docSidebar',
+					sidebarId: 'tutorialSidebar',
+					position: 'left',
+					label: 'Tutorial',
+				},
+				{
+					to: '/blog',
+					label: 'Blog',
+					position: 'left'
+				},
+				{
+					href: 'https://github.com/RGSS-CS/',
+					label: 'GitHub',
+					position: 'right',
+				},
+			],
 		},
 		footer: {
-		style: 'dark',
-		links: [
-			{
-			title: 'Docs',
-			items: [
+			style: 'dark',
+			links: [
 				{
-				label: 'Tutorial',
-				to: '/docs/intro',
+				title: 'Docs',
+				items: [
+					{
+					label: 'Tutorial',
+					to: '/docs/intro',
+					},
+				],
+				},
+				// {
+				// title: 'Community',
+				// items: [
+				// 	{
+				// 		label: 'Stack Overflow',
+				// 		href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+				// 	},
+				// 	{
+				// 		label: 'Discord',
+				// 		href: 'https://discordapp.com/invite/docusaurus',
+				// 	},
+				// 	{
+				// 		label: 'X',
+				// 		href: 'https://x.com/docusaurus',
+				// 	},
+				// ],
+				// },
+				{
+				title: 'More',
+				items: [
+					{
+						label: 'Blog',
+						to: '/blog',
+					},
+					{
+						label: 'GitHub',
+						href: 'https://github.com/RGSS-CS/documentation',
+					},
+				],
 				},
 			],
-			},
-			// {
-			// title: 'Community',
-			// items: [
-			// 	{
-			// 		label: 'Stack Overflow',
-			// 		href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-			// 	},
-			// 	{
-			// 		label: 'Discord',
-			// 		href: 'https://discordapp.com/invite/docusaurus',
-			// 	},
-			// 	{
-			// 		label: 'X',
-			// 		href: 'https://x.com/docusaurus',
-			// 	},
-			// ],
-			// },
-			{
-			title: 'More',
-			items: [
-				{
-				label: 'Blog',
-				to: '/blog',
-				},
-				{
-					label: 'GitHub',
-					href: 'https://github.com/RGSS-CS/documentation',
-				},
-			],
-			},
-		],
-		copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+			copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
 		},
 		prism: {
 			theme: prismThemes.github,
